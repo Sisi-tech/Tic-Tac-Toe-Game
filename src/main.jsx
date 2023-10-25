a6a82a5c72434d5120b952dbb06da2a07e14ab51
sisi wang <sisiwang242@gmail.com>
2023-10-24 22:36:42 -0400

    create Game function

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Board from './Board.jsx'
+import Game from './Board.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Board />
+    <Game />
   </React.StrictMode>,
 )
